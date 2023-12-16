7a8f74b312741734d59e306fede8484d6d7fdc10
Billy Laws <blaws05@gmail.com>
2023-12-16 11:37:56 -0800

    ntdll: Implement KiUserEmulationDispatcher for ARM64EC.
    This allows for direct resumption from an x64 context in cases like unwinding,
    without needing special handling on the PE caller side.

--- dlls/ntdll/unix/loader.c
+++ dlls/ntdll/unix/loader.c
@@ -111,6 +111,7 @@ void *pKiRaiseUserExceptionDispatcher = NULL;
 void *pKiUserExceptionDispatcher = NULL;
 void *pKiUserApcDispatcher = NULL;
 void *pKiUserCallbackDispatcher = NULL;
+void *pKiUserEmulationDispatcher = NULL;
 void *pLdrInitializeThunk = NULL;
 void *pRtlUserThreadStart = NULL;
 void *p__wine_ctrl_routine = NULL;
@@ -1531,6 +1532,7 @@ static void load_ntdll_functions( HMODULE module )
     GET_FUNC( KiUserExceptionDispatcher );
     GET_FUNC( KiUserApcDispatcher );
     GET_FUNC( KiUserCallbackDispatcher );
+    GET_FUNC( KiUserEmulationDispatcher );
     GET_FUNC( LdrInitializeThunk );
     GET_FUNC( LdrSystemDllInitBlock );
     GET_FUNC( RtlUserThreadStart );
@@ -1627,6 +1629,7 @@ static void redirect_ntdll_functions( HMODULE module )
     REDIRECT( KiUserExceptionDispatcher );
     REDIRECT( KiUserApcDispatcher );
     REDIRECT( KiUserCallbackDispatcher );
+    REDIRECT( KiUserEmulationDispatcher );
     REDIRECT( LdrInitializeThunk );
     REDIRECT( RtlUserThreadStart );
 #undef REDIRECT
