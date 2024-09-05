1b19d66139e0af4d39d5b3ed0714860546985aea
Billy Laws <blaws05@gmail.com>
2024-09-05 12:13:51 +0000

    fixup! ntdll/loader: add support for overriding IMAGE_FILE_LARGE_ADDRESS_AWARE

--- dlls/ntdll/signal_arm64ec.c
+++ dlls/ntdll/signal_arm64ec.c
@@ -476,6 +476,7 @@ DEFINE_SYSCALL(NtYieldExecution, (void))
 DEFINE_SYSCALL(wine_nt_to_unix_file_name, (const OBJECT_ATTRIBUTES *attr, char *nameA, ULONG *size, UINT disposition))
 DEFINE_SYSCALL(wine_unix_to_nt_file_name, (const char *name, WCHAR *buffer, ULONG *size))
 DEFINE_SYSCALL_(unsigned int, __wine_dbg_ftrace, (char *str, unsigned int str_size, unsigned int ctx))
+DEFINE_SYSCALL_(BOOL, __wine_needs_override_large_address_aware, (void))
 
 NTSTATUS SYSCALL_API NtAllocateVirtualMemory( HANDLE process, PVOID *ret, ULONG_PTR zero_bits,
                                               SIZE_T *size_ptr, ULONG type, ULONG protect )
