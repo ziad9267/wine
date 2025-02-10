2fce057d16e53aae8d375ca93e22c18757ef28c1
Paul Gofman <pgofman@codeweavers.com>
2025-02-10 15:44:26 -0600

    Revert "winex11: Present to the client window if there's any."
    This reverts commit 0ef83b6a99d368738319b3082ee8567854e0cb68.
    
    CW-Bug-Id: #24836

--- dlls/winex11.drv/window.c
+++ dlls/winex11.drv/window.c
@@ -2824,12 +2824,9 @@ Window get_onscreen_drawable( HWND hwnd, HWND toplevel, RECT *rect )
 
     if ((data = get_win_data( toplevel )))
     {
-        if (!(window = data->client_window))
-        {
-            OffsetRect( rect, data->rects.client.left - data->rects.visible.left,
-                        data->rects.client.top - data->rects.visible.top );
-            window = data->whole_window;
-        }
+        OffsetRect( rect, data->rects.client.left - data->rects.visible.left,
+                    data->rects.client.top - data->rects.visible.top );
+        window = data->whole_window;
         release_win_data( data );
         return window;
     }
